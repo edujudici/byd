d42e0e28b449d7b9a51a48066c874c1449daae75
Eduardo Judici <edujudici@gmail.com>
2018-02-27 15:32:35 -0300

    implementation send mail using mailgun

--- app/Http/Api/ContactServiceImpl.php
+++ app/Http/Api/ContactServiceImpl.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Api;
 
+use Mail;
 use App\Http\Api\ContactInterface;
 use App\Models\Company;
 use App\Models\Contact;
@@ -30,14 +31,36 @@ class ContactServiceImpl implements ContactInterface
         return response()->api($data);
     }
 
+    private function shootEmail()
+    {
+        $data = [
+            'title' => 'Dados do E-mail',
+            'name' => $this->contact->CON_NAME,
+            'lastname' => $this->contact->CON_LASTNAME,
+            'email' => $this->contact->CON_EMAIL,
+            'phone' => $this->contact->CON_PHONE,
+            'bodyMessage' => $this->contact->CON_MESSAGE,
+        ];
+
+        debug("send email to {$this->contact->CON_EMAIL}");
+        Mail::send('email.send', $data, function($message)
+        {
+            $message->from('edujudici@gmail.com', 'Eduardo');
+            $message->to('edujudici@gmail.com');
+        });
+        debug('email successfully sent!');
+    }
+
     public function send($request)
     {
         $this->contact->CON_NAME = $request->name;
-        $this->contact->CON_LASTNAME = $request->lastname;
+        $this->contact->CON_LASTNAME = $request->lastName;
         $this->contact->CON_EMAIL = $request->email;
         $this->contact->CON_PHONE = $request->phone;
         $this->contact->CON_MESSAGE = $request->message;
         $this->contact->save();
+
+        $this->shootEmail();
 
         return response()->api($this->contact);
     }
---
+++ resources/views/email/send.blade.php
@@ -0,0 +1,11 @@
+<html>
+	<head></head>
+	<body style="background: #232222; color: #6f6f6f; padding: 20px">
+		<h1 style="text-align: center">{{$title}}</h1>
+		<p><b>name: </b>{{ $name }}</p>
+		<p><b>lastname: </b>{{ $lastname }}</p>
+		<p><b>email: </b>{{ $email }}</p>
+		<p><b>phone: </b>{{ $phone }}</p>
+		<p><b>message: </b>{{ $bodyMessage }}</p>
+	</body>
+</html>
