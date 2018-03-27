cff53d16d696712cee27f8ff80b7364a5979158c
Eduardo Judici <edujudici@gmail.com>
2018-03-26 22:53:30 -0300

    implementation links to filter images when clicked.

--- app/Http/Api/PortifolioServiceImpl.php
+++ app/Http/Api/PortifolioServiceImpl.php
@@ -4,20 +4,24 @@ namespace App\Http\Api;
 
 use App\Http\Api\PortifolioInterface;
 use App\Models\Portifolio;
+use App\Models\PortfolioType;
 
 class PortifolioServiceImpl implements PortifolioInterface {
 
-	protected $portfolio;
+    protected $portfolio;
+	protected $portfolioType;
     
-    public function __construct(Portifolio $portfolio)
+    public function __construct(Portifolio $portfolio, PortfolioType $portfolioType)
     {
         $this->portfolio = $portfolio;
+        $this->portfolioType = $portfolioType;
     }
     
     public function getData()
     {
         $data = [
-            'portfolio' => $this->portfolio->all()
+            'portfolio' => $this->portfolio->all(),
+            'types' => $this->portfolioType->all()
         ];
         return response()->api($data);
     }
--- resources/views/portifolio.blade.php
+++ resources/views/portifolio.blade.php
@@ -23,32 +23,42 @@
 @endsection
 
 @section('content')
-    <section id="portfolio" class="container main">    
-        <ul class="gallery col-4">
-            <!-- ko foreach: portfolio-->
-            <li>
-                <div class="preview">
-                    <img alt="" data-bind="attr: {src: image}">
-                    <div class="overlay">
-                    </div>
-                    <div class="links">
-                        <a data-toggle="modal" data-bind="attr: {href: '#modal-'+$index()}"><i class="icon-eye-open"></i></a><a href="#"><i class="icon-link"></i></a>                                
-                    </div>
-                </div>
-                <div class="desc">
-                    <h5 data-bind="text: title"></h5>
-                    <small data-bind="text: description"></small>
-                </div>
-                <div class="modal hide fade" data-bind="attr: {id: 'modal-'+$index()}">
-                    <a class="close-modal" href="javascript:;" data-dismiss="modal" aria-hidden="true"><i class="icon-remove"></i></a>
-                    <div class="modal-body">
-                        <img data-bind="attr: {src: image}" alt=" " width="100%" style="max-height:400px">
-                    </div>
-                </div>                 
-            </li>
-            <!-- /ko -->
+    <section id="portfolio" class="container main"> 
 
-        </ul>
+        <div class="row-fluid" style="margin-bottom: 25px">
+            <!-- ko foreach: types -->
+                <button type="button" class="btn btn-primary btn-large" data-bind="text: POT_TITLE, click: $root.setType"></button>
+            <!-- /ko -->
+        </div>
+
+        <!-- ko foreach: portfolioGroup-->
+            <div class="row-fluid">
+                <!-- ko foreach: $data-->
+                    <ul class="gallery col-4">
+                        <li>
+                            <div class="preview">
+                                <img alt="" data-bind="attr: {src: image}">
+                                <div class="overlay">
+                                </div>
+                                <div class="links">
+                                    <a data-toggle="modal" data-bind="attr: {href: '#modal-'+$index()}"><i class="icon-eye-open"></i></a><a href="#"><i class="icon-link"></i></a>                                
+                                </div>
+                            </div>
+                            <div class="desc">
+                                <h5 data-bind="text: title"></h5>
+                                <small data-bind="text: description"></small>
+                            </div>
+                            <div class="modal hide fade" data-bind="attr: {id: 'modal-'+$index()}">
+                                <a class="close-modal" href="javascript:;" data-dismiss="modal" aria-hidden="true"><i class="icon-remove"></i></a>
+                                <div class="modal-body">
+                                    <img data-bind="attr: {src: image}" alt=" " width="100%" style="max-height:400px">
+                                </div>
+                            </div>                 
+                        </li>
+                    </ul>
+                <!-- /ko -->
+            </div>
+        <!-- /ko -->
     </section>
 @endsection
 
@@ -63,6 +73,7 @@
 
             self.title       = obj.POR_TITLE
             self.description = obj.POR_DESCRIPTION
+            self.type        = obj.POT_ID;
             self.image       = ko.observable();
 
             self.setImage = function(data)
@@ -82,6 +93,8 @@
             var self = this;
 
             self.portfolio = ko.observableArray();
+            self.types = ko.observableArray();
+            self.typeId = ko.observable();
 
             self.setData = function(response)
             {
@@ -90,8 +103,35 @@
                     self.portfolio(ko.utils.arrayMap(response.data.portfolio, function(obj) {
                         return new Portfolio(obj);
                     }));
+
+                    self.types(response.data.types);
+                    self.types.splice(0,0,{'POT_ID':0, 'POT_TITLE': 'TODOS'});
                 }
-            };            
+            };
+
+            self.portfolioGroup = ko.computed(function()
+            {
+                var lista;
+
+                if (!self.typeId() || self.typeId() == 0)
+                {
+                    lista = self.portfolio();
+                }
+                else
+                {
+                    lista = ko.utils.arrayFilter(self.portfolio(), function(item)
+                    {
+                        return item.type == self.typeId();
+                    });
+                }
+
+                return Api.groupList(lista, 4);
+            });   
+
+            self.setType = function(item)
+            {
+                self.typeId(item.POT_ID);
+            };
         }        
 
         var viewModel = new ViewModel();
